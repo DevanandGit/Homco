a1d03c360185f09082371371bbafab4e7aadf989
DevanandGit <nandhudevanand4419@gmail.com>
2023-05-24 13:40:28 +0530

    ready for deployment

--- homco/settings.py
+++ homco/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+8odfk687#r8j7kdcjxw%r1vlxi)@%%_q(di74eag-w@6t2j-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', 'Devanandjoly.pythonanywhere.com']
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
