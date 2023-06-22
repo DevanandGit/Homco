7625d8253d2781ddc08dcf9863cb528283b3dc84
DevanandGit <99065815+DevanandGit@users.noreply.github.com>
2023-06-22 23:49:41 +0530

    Update settings.py

--- homco/settings.py
+++ homco/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-%+8odfk687#r8j7kdcjxw%r1vlxi)@%%_q(di74eag-w@6t2j-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'Devanandjoly.pythonanywhere.com']
 
 
 # Application definition
